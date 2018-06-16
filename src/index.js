086ea02c057612c52859aed911840eaf8575385f
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2018-06-17 01:17:56 +0700

    feat(option): add optimization and transformation 🚸
    the options by default
    ```json
    {
      optimization: {
        level: 2,       // -O2
        shrinkLevel: 1  // -Os
      },
      transformation: {
        passes: [
          "duplicate-function-elimination",
          "inlining-optimizing",
          "remove-unused-module-elements",
          "memory-packing"
        ]
      },
      debug: false
    }
    ```

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import assert from 'assert';
 
 import { getOptions } from 'loader-utils';
 import validate from '@webpack-contrib/schema-utils';
-import { readBinary, setDebugInfo } from 'binaryen';
+import {
+  readBinary,
+  setOptimizeLevel,
+  setShrinkLevel,
+  setDebugInfo,
+} from 'binaryen';
 
 import schema from './options.json';
 
@@ -20,7 +25,43 @@ export default function loader(source) {
     target: options,
   });
 
-  // `..|| false` since docs not clear enough about the default value
+  if (options.optimization) {
+    const o = options.optimization;
+    if (o.level) setOptimizeLevel(o.level);
+    if (o.shrinkLevel) setShrinkLevel(o.shrinkLevel);
+  }
+
+  if (options.transformation) {
+    // #region helpers function
+    const tf = options.transformation;
+    const runOnFunction = (passes) => {
+      if (tf.function) wasmModule.runPassesOnFunction(tf.function, passes);
+    };
+    const runPassses = (passes) => {
+      wasmModule.runPasses(passes); // ⬅ ️since it only accept array (based on docs)
+      runOnFunction(passes);
+    };
+    // #endregion
+
+    // #region global transformation
+    if (tf.passes) {
+      if (Array.isArray(tf.passes)) runPassses(tf.passes);
+      else runPassses([tf.passes]); // ⬅ ️also accept string
+    }
+    // WARNING: docs not clear what default level ⤵️ optimization
+    else if (tf.function) wasmModule.optimizeFunction(tf.function); // ⬅ ️run default passes
+    // #endregion
+
+    // #region transformation on each specific functions
+    // if (Array.isArray(options.transformation)) {
+    //   // TODO: support different transformation on different function
+    //   /* TRACK: waiting binaryen.js support runPasses on different function
+    //     currently it support on single function or global transformation */
+    // }
+    // #endregion
+  }
+
+  // `..|| false` ⤵️ since docs not clear enough about the default value
   setDebugInfo(options.debug || false);
   wasmModule.optimize();
 
